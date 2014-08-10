94e760792b0059c80502c0923851835aeeaa14ae
Beren Oğuz <berenoguz@superonline.com>
2014-08-10 22:12:36 +0300

    issue#4 vulpes::Variable constructors

---
+++ include/vulpes/Variable.hpp
@@ -0,0 +1,11 @@
+namespace vulpes
+{
+    class Variable
+    {
+        private:
+            const Type& type;
+
+        public:
+            Variable(const Type& type);
+    };
+}
---
+++ src/Variable.cpp
@@ -0,0 +1,10 @@
+#include "vulpes.hpp"
+
+namespace vulpes
+{
+    Variable::Variable(const Type& type)
+    : type(type)
+    {
+
+    }
+}
